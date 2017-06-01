d9d469da720c630f999d7cdb03c03f785dce04a2
Leonard Siebeneicher <leonard7e@gmail.com>
2017-06-01 13:54:56 +0200

    postcss

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,27 @@
 var gulp = require("gulp");
+// Load modules here
 var ts = require("gulp-typescript");
+var postcss = require('gulp-postcss');
+
+// Task – TypeScript
 var tsProject = ts.createProject("tsconfig.json");
 
-gulp.task("default", function () {
+gulp.task("typescript", function () {
     return tsProject.src()
         .pipe(tsProject())
-        .js.pipe(gulp.dest("static/js-gen"));
+        .js.pipe(gulp.dest("./static/js-gen"));
 });
+
+// Task – PostCSS
+
+// var autoprefixer = require('autoprefixer');
+// var cssnano = require('cssnano');
+
+gulp.task('postcss', function () {
+    return gulp.src('./src/postcss/*.css')
+        .pipe(postcss())
+        .pipe(gulp.dest('./static/css'));
+});
+
+// Task – Default
+gulp.task('default', ['typescript','postcss']);
