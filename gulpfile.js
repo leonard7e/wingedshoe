e5aceadb0de3ba7036a39a4002267bbe578c58e2
Leonard Siebeneicher <leonard7e@gmail.com>
2017-06-08 23:06:26 +0200

    Going for a solid foundation

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,44 @@
 var gulp = require("gulp");
-// Load modules here
-var ts = require("gulp-typescript");
-var postcss = require('gulp-postcss');
+
+var webpack = require("gulp-webpack");
+gulp.task('ts', function() {
+  return gulp.src('src/typescript/wingedshoe.tsx')
+    .pipe(webpack(require('./webpack.config.js') ))
+    .pipe(gulp.dest('static/js-gen/'));
+});
 
 // Task – TypeScript
-var tsProject = ts.createProject("tsconfig.json");
-
-gulp.task("typescript", function () {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest("./static/js-gen"));
-});
+// var ts = require("gulp-typescript");
+// var tsProject = ts.createProject("tsconfig.json");
+//
+// gulp.task("ts", function () {
+//     return tsProject.src()
+//         .pipe(tsProject())
+//         .js.pipe(gulp.dest("./static/js-gen"));
+// });
 
 // Task – PostCSS
 
 // var autoprefixer = require('autoprefixer');
 // var cssnano = require('cssnano');
 
+var postcss = require('gulp-postcss');
+
 gulp.task('postcss', function () {
+    var sourcemaps = require('gulp-sourcemaps');
     return gulp.src('./src/postcss/*.css')
-        .pipe(postcss())
+        .pipe(sourcemaps.init())
+        .pipe(postcss([ require('precss'), require('autoprefixer') ]))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./static/css'));
 });
 
+var livereload = require('gulp-livereload');
+gulp.task('watch', function () {
+  livereload.listen();
+  gulp.watch('./src/typescript/*.tsx', ['ts']);
+  gulp.watch('./src/postcss/*.css', ['postcss']);
+})
+
 // Task – Default
-gulp.task('default', ['typescript','postcss']);
+gulp.task('default', ['watch']);
