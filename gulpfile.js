ab6c4c298031d77bf8a69562d1524d4cda78e38d
Leonard Siebeneicher <leonard7e@gmail.com>
2019-12-07 09:11:50 +0100

    Get rid of Vue

--- gulpfile.js
+++ gulpfile.js
@@ -2,25 +2,10 @@ var gulp = require("gulp");
 
 var webpack = require("gulp-webpack");
 gulp.task('ts', function() {
-  return gulp.src('src/typescript/wingedshoe.tsx')
+  return gulp.src('src/typescript/index.ts')
     .pipe(webpack(require('./webpack.config.js') ))
     .pipe(gulp.dest('static/js-gen/'));
 });
-
-// Task – TypeScript
-// var ts = require("gulp-typescript");
-// var tsProject = ts.createProject("tsconfig.json");
-//
-// gulp.task("ts", function () {
-//     return tsProject.src()
-//         .pipe(tsProject())
-//         .js.pipe(gulp.dest("./static/js-gen"));
-// });
-
-// Task – PostCSS
-
-// var autoprefixer = require('autoprefixer');
-// var cssnano = require('cssnano');
 
 var postcss = require('gulp-postcss');
 
@@ -30,17 +15,6 @@ gulp.task('postcss', function () {
         .pipe(sourcemaps.init())
         .pipe(postcss([
           require('postcss-import'),
-          // require('postcss-nested'),
-          require('postcss-color-function'),
-          require('postcss-simple-vars'),
-          // require('postcss-calc'),
-          // require('postcss-conditionals'),
-          // require('postcss-each'),
-          // require('postcss-for'),
-          // require('postcss-sass-extend'),
-          // require('postcss-mixins'),
-          // require('postcss-sprites'),
-          require('autoprefixer')
         ]))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./static/css-gen'));
@@ -49,7 +23,7 @@ gulp.task('postcss', function () {
 var livereload = require('gulp-livereload');
 gulp.task('watch', function () {
   livereload.listen();
-  gulp.watch(['./src/typescript/*.tsx','./src/typescript/**/*.tsx'], ['ts']);
+  gulp.watch(['./src/typescript/*.ts','./src/typescript/**/*.ts'], ['ts']);
   gulp.watch(['./src/postcss/*.css', './src/postcss/**/*.css'], ['postcss']);
 })
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,20 @@
 module.exports = {
-
-    entry: "./src/typescript/wingedshoe",
+    entry: "./src/typescript/index",
     output: {
         filename: "bundle.js",
         path: __dirname + "/static/js-gen"
     },
     devtool: "source-map",
     resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
         extensions: [".ts", ".tsx", ".js", ".json"]
     },
 
     module: {
       loaders: [
-        { test: /\.tsx?$/, loader: "ts-loader" },
-        { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+        {
+            test: /\.ts?$/, loader: "ts-loader" },
+        {
+            enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
       ]
-    //    rules: [
-    //        // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-    //        { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-
-    //        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-    //    ]
     },
-
-
-    // externals: {
-    //   "angular": "Angular"
-    // },
 };
